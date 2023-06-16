41bda8d80139da8e3f59730a82a875c64562ab06
Rosapedret2 <rosapedret2@gmail.com>
2023-06-16 14:50:33 +0200

    estructura y estilo de header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,16 @@
 import "./Header.css";
-
+import logo from "../../assets/logo.svg"
 export default function Header() {
     return (
-        <div className="Header">
-            <h4>Header</h4>
+        <div className="header">
+            <div className="logo-container">
+                <img  className="img-logo" src={logo} alt="Logo Sound waves" />
+                <p className="text-logo">SoundWave</p>
+            </div>
+            <div className="buttons-container">
+                <button className="header-btn">Discover</button>
+                <button className="header-btn">Join</button>
+            </div>
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/header/Header"
 export default function Home() {
     return (
         <div className="home">
-            <h4>Home</h4>
+            
             <div className="home-header">
                 <Header />
             </div>
