e716aa7a1479ca7c9d42dd2362885dfdc69b281e
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-06-20 13:00:47 +0200

    MainDiscover progress

--- src/components/main/MainDiscover.jsx
+++ src/components/main/MainDiscover.jsx
@@ -1,9 +1,24 @@
 import "./MainDiscover.css"
+import Charts from "../../assets/Charts.svg"
+import Albums from "../../assets/Albums-container.svg"
+import More from "../../assets/more-container.svg"
+import Covers from "../../assets/covers.jpg"
 
 export default function MainDiscover(){
     return(
         <div className="maindiscover-container">
-        Discover
+        <div className="text-container">
+            <h3>Discover new music</h3>
+            <div className="icons-container">
+                <img src= {Charts}  alt="Icono microfono" />
+                <img src= {Albums}  alt="Icono Albums" />
+                <img src= {More}  alt="Icono More" />
+            </div>
+            <p>By joing you can benefit by listening to the latest albums released</p>
+        </div>
+        <div className="covers-img">
+            <img src= {Covers} alt="Covers" />
+        </div>
         </div>
     )  
 }
